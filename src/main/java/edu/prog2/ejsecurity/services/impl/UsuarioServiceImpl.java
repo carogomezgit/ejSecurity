9c732641dc3eeaff2f057187d253174b44de6fcb
Ara <aragomezgd@gmail.com>
2026-09-14 20:34:19 -0300

    add service annotation

--- src/main/java/edu/prog2/ejsecurity/services/impl/UsuarioServiceImpl.java
+++ src/main/java/edu/prog2/ejsecurity/services/impl/UsuarioServiceImpl.java
@@ -1,12 +1,13 @@
 package edu.prog2.ejsecurity.services.impl;
 
-import edu.prog2.ejsecurity.model.Usuario;
 import edu.prog2.ejsecurity.repositories.UsuarioRepository;
 import edu.prog2.ejsecurity.services.UsuarioService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioServiceImpl implements UsuarioService, UserDetailsService {
   /// INYECCIÓN DE DEPENDENCIAS: AGREGO EL REPOSITORY
   private final UsuarioRepository usuarioRepository;
